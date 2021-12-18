0925bdbc12e0f8e50eefec06ce0e9c1d26553b75
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-12-18 21:44:43 +0000

    Updated requirements.txt & customized admin

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -104,10 +104,9 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
