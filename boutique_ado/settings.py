8a905f8b4ef87190f1b7b802e6db9b101af11904
yetnetbehailu <yetnet.behailu@hotmail.com>
2022-02-13 19:18:52 +0000

    sending real emails- misspelling corrected

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -208,7 +208,7 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    Email_USE_TLS = True
+    EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
